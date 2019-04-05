c8b7a6e2edc86045bb893df5b52e6fe46b424bc3
Miguel Luis Neder <mneder@spamarrest.com>
2019-04-04 20:25:09 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div onClick={this.props.onClick}>We have manufactured <strong>{this.props.clicks}</strong> Terminators.
+         Let's make some more by clicking here... </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+let model = {clicks:5};
+function render(){
+    ReactDOM.render(<App
+                    clicks={model.clicks}
+                    onClick={() => { ++model.clicks; render();  } }/>, document.getElementById('root'));
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+}
+render();
 serviceWorker.unregister();
